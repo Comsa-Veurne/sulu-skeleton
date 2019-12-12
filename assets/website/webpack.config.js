ab479837fe21e68d948a1367c58da4e73634ec93
Ciryk Popeye <ciryk@hotmail.be>
2019-12-12 14:33:20 +0100

    Add webpack and Bootstrap

---
+++ assets/website/webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const DEV_MODE = 'development';
+const PROD_MODE = 'production';
+const PORT = 8080;
+
+module.exports = (env, args) => {
+    let mode = args.development ? DEV_MODE : PROD_MODE;
+
+    let config = {
+        mode,
+        plugins: [
+            new ManifestPlugin({
+                writeToFileEmit: true
+            })
+        ],
+        entry: {
+            'app': './src/index.js'
+        },
+        output: {
+            path: path.resolve(__dirname, '../../public/build/website'),
+            filename: '[name]-[hash].js',
+            publicPath: mode === 'development' ? 'http://localhost:' + PORT + '/build/website/' : '/build/website/'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: ['babel-loader']
+                },
+                {
+                    test: /\.scss$/,
+                    use: mode === DEV_MODE ? [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ] : ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            'css-loader', 'sass-loader'
+                        ]
+                    }),
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    loaders: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 0
+                            }
+                        }
+                    ]
+                }
+            ],
+        },
+        devServer: {
+            contentBase: path.join(__dirname, '../../public/build/website'),
+            compress: true,
+            port: PORT
+        }
+    };
+
+    if (mode === DEV_MODE) {
+        config.plugins.push(
+          new StyleLintPlugin({
+              files: 'src/scss/**/*.scss'
+          })
+        );
+    } else {
+        config.plugins.push(
+          new ExtractTextPlugin({
+              filename: '[name]-[hash].css'
+          }),
+          new CleanWebpackPlugin()
+        );
+    }
+
+    return config;
+};
