a47263c760202fa74e966693cb44814046ef1458
Ciryk Popeye <ciryk@hotmail.be>
2020-01-06 14:58:31 +0100

    Default implementation mobile nav

--- assets/website/src/index.js
+++ assets/website/src/index.js
@@ -1,2 +1,9 @@
 import './scss/index.scss';
 
+//-- Mobile navigation
+document.querySelector('.nav-mobile-toggle').addEventListener('click', (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  e.target.classList.toggle('is-active');
+  document.querySelector('.nav-mobile').classList.toggle('is-open');
+});
