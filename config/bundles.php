a7313f96eaffedfa2f9fc16b43c380b2fd2aad23
Ciryk Popeye <ciryk@hotmail.be>
2020-04-03 12:16:44 +0200

    Working spam free bundle

--- config/bundles.php
+++ config/bundles.php
@@ -44,5 +44,5 @@ return [
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true, 'admin' => true],
     Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle::class => ['all' => true, 'website' => true],
     Sulu\Bundle\FormBundle\SuluFormBundle::class => ['all' => true],
-    EWZ\Bundle\RecaptchaBundle\EWZRecaptchaBundle::class => ['all' => true],
+    Comsa\SuluSpamFreeBundle\ComsaSuluSpamFreeBundle::class => ['all' => true],
 ];
