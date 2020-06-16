f2ad1b31020a9ca2aa26808f851cad4374f6be3b
Gilles Knockaert <gilles@comsa.be>
2020-06-16 14:55:50 +0200

    added lightbox and masonry to skeleton

--- assets/website/src/index.js
+++ assets/website/src/index.js
@@ -4,3 +4,23 @@ import './scss/index.scss';
 //-- Load modules
 import './components/nav';
 import './components/video';
+
+//-- masonry
+import imagesLoaded from 'imagesloaded';
+import Masonry from 'masonry-layout';
+
+//--Lightbox
+import GLightbox from 'glightbox';
+
+const images = document.getElementsByClassName('images');
+imagesLoaded(document.querySelector('body'), function () {
+  const n = images.length;
+  for (var i = 0; i < n; i++) {
+    new Masonry(images[i], {
+      itemSelector: 'div',
+      //gutter: 15
+    });
+  }
+});
+
+GLightbox();
